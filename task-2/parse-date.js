c5da2d144ae8343faebd00364e9ece3989deb296
dagimgit config --global user.email dagimvanhaileselassie@gmail.com <hackyourfuture@HackYourFutures-MacBook-Pro.local>
2026-01-28 16:45:01 +0100

    Implement date conversion module

--- task-2/parse-date.js
+++ task-2/parse-date.js
@@ -23,7 +23,6 @@ function parseDateString(dateString) {
         year: y
     };
 }
-
 console.log(parseDateString("MDY 10-21-1983")); 
 console.log(parseDateString("DMY 21-10-1983"));
 console.log(parseDateString("MDY 03-15-2024"));
